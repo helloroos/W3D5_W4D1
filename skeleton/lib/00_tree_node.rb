b103b5901697e34231f6ae4e5f445cdca9a7d9c9
akerpelm <akerpelm@u.rochester.edu>
2021-03-12 14:56:15 -0500

    Add non-functioning parent class

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,11 +1,20 @@
 class PolyTreeNode
 
     attr_reader :value, :parent, :children
+    
     def initialize(value)
         @value = value
         @parent = nil
         @children = []
+    end
 
+    def parent=(node)
+        node.children.delete(self) unless node.parent.nil?
+        @parent = node
+        @parent.children << self unless parent.children.include?(self)
     end
 
 end
+
+# node1 = PolyTreeNode.new('root')
+
