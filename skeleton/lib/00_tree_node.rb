2462cf25916f14f2e5f05cf4819cd38199669c80
michelleroos <hello.michelleroos@gmail.com>
2021-03-12 15:16:15 -0500

    Created a parent= method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -8,13 +8,26 @@ class PolyTreeNode
         @children = []
     end
 
-    def parent=(node)
-        node.children.delete(self) unless node.parent.nil?
-        @parent = node
-        @parent.children << self unless parent.children.include?(self)
+    def parent=(new_parent)
+        parent.children.delete(self) unless @parent.nil?
+        @parent = new_parent
+        parent.children << self unless parent.children.include?(self)
     end
 
 end
 
 # node1 = PolyTreeNode.new('root')
+# node2 = PolyTreeNode.new('child1') 
+# node3 = PolyTreeNode.new('child2') 
 
+# node4 = PolyTreeNode.new('new_parent')
+
+# node1.parent = node4
+
+# Remove ourselves from old parent's children's_arr
+# Reassign our parent
+# Add ourselves to new_parent's arr
+
+# Nil?? 
+
+# Does node1 have a parent? 
