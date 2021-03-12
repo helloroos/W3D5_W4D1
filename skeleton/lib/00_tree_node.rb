732d55e9645f42b0063ccdce1774b65a9e9ea9fc
akerpelm <akerpelm@u.rochester.edu>
2021-03-12 16:55:17 -0500

    Start dfs method

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -4,7 +4,7 @@ class PolyTreeNode
     
     def initialize(value)
         @value = value
-        @parent = nil # Node1
+        @parent = nil
         @children = []
     end
 
@@ -19,11 +19,33 @@ class PolyTreeNode
     end
 
     def remove_child(child)
-        if @children.include?(child)
-            child.parent = nil
+        if children.include?(child)
+            child.parent = nil 
         else
-            raise "Error!"
+            raise 'Error'
         end
+    end
+
+    def push(node)
+    
+    end
+
+    def dfs(target) # 'a', "g"
+        # Run through one side of the tree at a time
+        return nil if self.value == nil
+        return self.value if self.value == target #'g'
+    
+        self.children.each do |child|
+            search_result = dfs(child, target)
+            return search_result unless search_result == nil
+    
+        end
+    
+        #children array = [b [d, e], c[f,g]]
+            # call dfs recursively on the children
+            # if we return a val, that's great
+            # if nil, recursively call on the remaining children
+        nil
     end
 
 end
