e24ec2342a4ee18558944a143d4814b812659d46
michelleroos <hello.michelleroos@gmail.com>
2021-03-15 13:39:55 -0400

    CInitialized class and created two class methods

--- knights_travails/KnightPathFinder.rb
+++ knights_travails/KnightPathFinder.rb
@@ -1,1 +1,39 @@
-hello
+require_relative "./skeleton/00_tree_node.rb"
+
+class KnightPathFinder # FIND SHORTEST PATH
+
+    def self.root_node
+        PolyTreeNode.new(start_pos) # [0, 0]
+    end
+
+    def self.valid_moves?(pos)
+        moves 
+    end
+
+    attr_reader :start_pos
+
+    def initialize(start_pos)
+        @start_pos = start_pos
+        @move_tree = [] # call move_tree (class method)
+    end
+
+end
+
+[
+    [00, ] 0
+    [] 1
+    [] 2
+    [] 3
+    [] 4
+    [] 5
+    [] 6
+    [] 7
+]
+
+# Start at root = start_pos
+# Find all valid positions that a knight can move // BFS
+# From that element we create a parent element
+# Then we look at every possible move from there
+# KEep doing that until we exhausted every poss
+
+# Traverse > row, col > when col == 7 > return 
